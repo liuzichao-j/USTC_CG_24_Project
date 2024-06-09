acc2fe258a9520521e668adfd2fac139a69cd705
rubatotree <zhuyutian@mail.ustc.edu.cn>
2024-06-09 14:54:49 +0800

    Fix Newton Iteration Grad

--- Framework3D/source/RCore/hd_USTC_CG_GL/geometries/mesh.cpp
+++ Framework3D/source/RCore/hd_USTC_CG_GL/geometries/mesh.cpp
@@ -292,7 +292,7 @@ void Hd_USTC_CG_Mesh::Sync(
                 double d = pxr::GfDot(x - camera_position, beta);
 
                 double f = d - c * (t - time);
-                double df = GfDot(x - camera_position, v) - c;
+                double df = GfDot((x - camera_position).GetNormalized(), v) - c;
                 double step = 1;
                 if (df != 0)
                     step = f / df * damping;
